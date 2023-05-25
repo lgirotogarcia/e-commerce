1e696d3cba38316d918aae872f0ffc2c88e787f1
Leonardo Garcia <leonardogirotogarcia@gmail.com>
2023-05-25 16:29:06 -0300

    Adição do módulo LandingPageComponent e alteração dos estilos para corrigir botões e cores do site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from '@angular/material/card'
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from '@routes/app-routing.module';
@@ -9,20 +10,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { LandingPageComponent } from './core/pages/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavBarComponent
+    NavBarComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
     MatIconModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
